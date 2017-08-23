640431f7675d08010c141214508c58c36a5b2504
siliconshecky <mbkavka@siliconshecky.com>
2017-08-23 10:41:04 -0500

    Add files via upload

---
+++ GetEventIDs.ps1
@@ -0,0 +1,8 @@
+﻿# This script will ask for parameters to look for Event IDs inside of the windows Logs
+
+$machine = read-host -prompt "Machine name"
+$id = read-host -prompt "Event ID"
+$logname = read-host -prompt "Log Name"
+Get-Eventlog -Logname $logname -ComputerName $machine |
+Where-Object {$_.EventID -eq $id} |
+Format-Table MachineName, Source, EventID -auto
