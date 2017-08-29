6e2ad661cfd7a7d4715dbbeead6b4b4ccaa08ffd
siliconshecky <mbkavka@siliconshecky.com>
2017-08-29 08:19:48 -0500

    Add files via upload

---
+++ User Property Matching.ps1
@@ -0,0 +1,11 @@
+﻿# This script will read a CSV file of User Logon names and rename the SamAccountName to match the UPN
+# You can easily modify the paramters to change any account property. For instance flipping the samaccountanme and userprincipalname you can flip which is being replaced.
+
+$Users = Import-Csv c:\csv\usersfull.csv
+Foreach ($user in $users) {
+                $upn = (get-aduser $user.samaccountname -properties userprincipalname).userprincipalname
+                $upn = ($upn -split “@”)[0]
+                    write-host $upn
+               set-aduser -identity $user.samaccountname -Replace @{samaccountname=$upn}
+               
+}
