b023a205053803c09cde926cfdba9ef6017d370d
siliconshecky <mbkavka@siliconshecky.com>
2017-09-06 10:33:30 -0500

    Update 90daysInactiveAccountCheck.ps1

--- 90daysInactiveAccountCheck.ps1
+++ 90daysInactiveAccountCheck.ps1
@@ -1,4 +1,4 @@
 ﻿Search-ADAccount -UsersOnly -Search "ou=<OU>,dc=<DC>,dc=<DC>,dc=com" -AccountInactive -TimeSpan "105" |
  Get-ADUser -Properties Name, sAMAccountName, givenName, sn, userAccountControl, lastlogondate | 
  Where {($_.userAccountControl -band 2) -eq $False} | Select Name, sAMAccountName, givenName, sn, lastlogondate | 
- export-csv C:\CSV\TriEd90daysinactive.csv
+ export-csv C:\CSV\90daysinactive.csv
