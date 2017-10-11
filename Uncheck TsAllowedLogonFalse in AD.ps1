36cccfa75b54c44cfe4df29235dc4abf6777554c
siliconshecky <mbkavka@siliconshecky.com>
2017-10-11 14:47:25 -0500

    Add files via upload

--- Uncheck TsAllowedLogonFalse in AD.ps1
+++ Uncheck TsAllowedLogonFalse in AD.ps1
@@ -1,15 +1,16 @@
-# This script removes the checkmark from the Do Not Allow Terminal Services Logon box in AD
-# From all users in a OU.
+#This script will uncheck the Do Not Allow Terminal Services Logon for User accounts
+
+
 Import-Module ActiveDirectory
 $out = @()
 $searchOU = "OU=Users,OU=whatever,DC=domain,DC=com"
 (Get-ADuser -filter * -searchbase $searchOU -properties DistinguishedName) | ForEach `
   {
     $userDN = $_ | Select -expandProperty DistinguishedName
-    $user = [adsi]"LDAP://$userDN" #get user information from LDAP since the attribute is not accessible from AD
+    $user = [adsi]"LDAP://$userDN" #this gets the user to equate to the LDAP equivalent
     if($user.AllowLogon -ne 1){
-    $user.psbase.invokeSet(“allowLogon”,1) #Change the attribute to allow logon from do not allow
-    $user.CommitChanges()
-    Write-Host "$($user.Name) has been granted access to RDS" -fore green
+    $user.psbase.invokeSet(“allowLogon”,1) #invokeSet and allowlogon ar eused in conjustion with LDAP because the Attribute is not available with a straight AD query
+    $user.CommitChanges() #Commit the changes made to the opbject (pressing apply in the GUI)
+    Write-Host "$($user.Name) has been granted access to RDS" -fore green #write the output of the changes to the screen. You could set this up for a CSV output also.
 }
   }
