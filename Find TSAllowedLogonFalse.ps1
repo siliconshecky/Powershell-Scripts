10504904f477db073c6e5f912a1b665efe322f49
siliconshecky <mbkavka@siliconshecky.com>
2017-10-11 14:45:29 -0500

    Add files via upload

--- Find TSAllowedLogonFalse.ps1
+++ Find TSAllowedLogonFalse.ps1
@@ -1,5 +1,5 @@
-# This script will Find all users in an OU that have the Do Not Allow Logon to Terminal Sessions checkbox checked
-# Which prevents them from using RDP or Citrix
+# This script will go through and find AD Users who have the
+# Do Not allow Login to terminal sessions box checked
 
 Import-Module ActiveDirectory
 $out = @()
@@ -7,15 +7,15 @@ $searchOU = "OU=Users,OU=whatever,DC=domain,DC=com"
 (Get-ADuser -filter * -searchbase $searchOU -properties DistinguishedName) | ForEach `
   {
     $userDN = $_ | Select -expandProperty DistinguishedName
-    $user = [adsi]"LDAP://$userDN"
-    If ($user.AllowLogon -eq "0" )
+    $user = [adsi]"LDAP://$userDN" #this gets the user to equate to the LDAP equivalent
+    If ($user.AllowLogon -eq "0" ) #invokeSet and allowlogon ar eused in conjustion with LDAP because the Attribute is not available with a straight AD query
     {
       $Properties = @{
         Name = "$($_.Name)"
-        sAMAccountName = "$($_.sAMAccountName)"
+        sAMAccountName = "$($_.sAMAccountName)" #pull the Pre-windows 2000 User ID and Full name of the account
       }
-      $custom = New-Object PSObject -property $Properties
-      $out += $custom
+      $custom = New-Object PSObject -property $Properties #pulls the Properties using AD so it can be added to the output
+      $out += $custom #add the Name and User ID to the current output matrix to be exported to a CSV file
     }
   }
 $out | Select Name,sAMAccountName | Export-Csv -NoTypeInformation "C:\results\TsAllowLogonFalse.csv"
