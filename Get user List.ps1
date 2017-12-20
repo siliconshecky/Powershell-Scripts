7a322b913c35207ea8a96f8333bd3862719d3f2f
siliconshecky <mbkavka@siliconshecky.com>
2017-12-20 09:01:27 -0600

    Add files via upload
    Gets lists of users and their properties from AD

---
+++ Get user List.ps1
@@ -0,0 +1,6 @@
+﻿# You can change the filter from sAMAccountName to any attribute you like. The more unique the -like part is the more targeted (and fewer) results you will get
+# Feel free to add/subtract/change properties selected
+	
+Get-ADUser -Filter {sAMAccountName -like '*'} -Properties Name, SamAccountName, Description, whenCreated, manager | 
+    select-object Name, SamAccountName, Description, whenCreated,@{label="Manager";expression={$_.manager -replace '^CN=|,.*$'}} | 
+    export-csv "c:\csv\Contractor List.csv"
