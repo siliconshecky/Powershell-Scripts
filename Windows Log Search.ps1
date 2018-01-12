518c421e0765d2879f40c4502b0801345917e417
siliconshecky <mbkavka@siliconshecky.com>
2018-01-12 09:29:08 -0600

    Add files via upload

--- Windows Log Search.ps1
+++ Windows Log Search.ps1
@@ -2,11 +2,11 @@
 # this script will not work in Posershell ISE. Run from normal Powershell only.
 
 Write-host "To select logs from the microsoft folder under Application and Services logs use the following format: Microsoft-Windows-Sysmon/Operational" -foregroundcolor magenta
-Write-Host "Output file names do not need an extantion, just a name for the csv file." -foregroundcolor magenta
+Write-Host "Output file names do not need an extention, just a name for the csv file." -foregroundcolor magenta
 Write-host "Date format should be mm/dd/yyyy. Single digit months do not need a 0 (ie 1 for Jan not 01)" -foregroundcolor magenta
 Write-host "Start and end date must be different days (i.e. 1/12/2018 to 1/12/2018 won't work but 1/11/2018 to 1/122018 will)" -foregroundcolor magenta
 Write-host "File path is the base, it will append the machine name (if there is one) plus tthe file name inputted" -foregroundcolor magenta
-
+Write-host "Separate multiple event Ids with a comma"
 #get inputs from user
 $NameOfLog = Read-Host -Prompt 'What Windows Log are we searching:'
 $MachineName = Read-Host -Prompt 'Please enter machine name or blank for local machine' # gets machine name
